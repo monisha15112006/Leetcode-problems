c5bbb369a0172c14027d906dc4000be7a4e2c671
monisha15112006 <monisha15112006_bcs28@mepcoeng.ac.in>
2026-04-13 22:51:18 +0530

    Update Search insert position.java

--- Search insert position.java
+++ Search insert position.java
@@ -1,49 +1,20 @@
-import java.util.*;
-
 class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        
-        int n = sc.nextInt();
-
-        int[] nums = new int[n];
-
-        
-        
-        for (int i = 0; i < n; i++) {
-            nums[i] = sc.nextInt();
-        }
-
-        // Input value to remove
-        
-        int val = sc.nextInt();
-
-        // Call function
-        int k = removeElement(nums, val);
-
-        // Output result
-        System.out.println(k);
-        
-
-        for (int i = 0; i < k; i++) {
-            System.out.print(nums[i] + " ");
-        }
-
-        sc.close();
-    }
-
-    // Function to remove elements
-    public static int removeElement(int[] nums, int val) {
-        int k = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[k] = nums[i];
-                k++;
+    public int searchInsert(int[] nums, int target) {
+        int low =0;
+        int high=nums.length-1;
+        int ans =nums.length;
+        while(low<=high){
+            int mid =(low+high)/2;
+            if(nums[mid]>=target){
+                ans=mid;
+                high=mid-1;
             }
-        }
+            else{
+                low =mid+1;
+            }
 
-        return k;
+        }
+        return ans;
+        
     }
 }
