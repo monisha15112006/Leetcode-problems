ed27b3f7f50d2cd623a9f4cee61d65db1c2d7568
monisha15112006 <monisha15112006_bcs28@mepcoeng.ac.in>
2026-04-13 22:50:29 +0530

    Update Remove Duplicates from Sorted list.java

--- Remove Duplicates from Sorted list.java
+++ Remove Duplicates from Sorted list.java
@@ -1,14 +1,15 @@
 class Solution {
-    public int[] plusOne(int[] digits) {
-        int[] arr = new int[digits.length + 1];
-        arr[0] = 1;
-        for(int i = digits.length - 1; i >= 0; i--) {
-            if(digits[i] < 9) {
-                digits[i]++;
-                return digits;
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode curr = head;
+
+        while (curr != null && curr.next != null) {
+            if (curr.val == curr.next.val) {
+                curr.next = curr.next.next; // remove duplicate
+            } else {
+                curr = curr.next; // move forward
             }
-            digits[i] = 0;
         }
-        return arr;
+
+        return head;
     }
 }
